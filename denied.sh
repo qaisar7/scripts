b9fe696e904fca147c4d99c38a7b7d367e03d772
Qaisar <qaisar.sardar@gmail.com>
2020-03-26 14:06:25 +1100

    1. Add time based login where users can't login before a certain time. 2. Add denied.sh file which lists the recent 'CONNECT' or 'DENIED' sites for a user.

---
+++ denied.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+U=$1
+M="DENIED"
+
+if [[ $1 =~ '-v' ]]; then
+	U=$2
+	# If -v is specified as the first arugment then look for all the connections.
+	M="CONNECT"
+fi
+echo "$M $U"
+
+sites=`sudo cat /var/log/squid3/access.log | grep "$M".*"$U" | awk '{print $7}' | sort -u | uniq`
+
+for i in $sites;do
+	# Get the most recent time for the match.
+	time=`sudo grep -m1 "$M".*"$i".*"$U" /var/log/squid3/access.log | awk '{print $1}' | cut -d. -f1`
+	# Convert the time from epoxh to Human readable format.
+	echo `date -d @$time` $i
+done
