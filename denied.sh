5ee4a211d037abb9a9b117cbdf4d3ac0e8cc07b0
Qaisar <qaisar.sardar@gmail.com>
2020-03-26 14:31:35 +1100

    Fix reading the log file by reading from the end

--- denied.sh
+++ denied.sh
@@ -14,7 +14,7 @@ sites=`sudo cat /var/log/squid3/access.log | grep "$M".*"$U" | awk '{print $7}' 
 
 for i in $sites;do
 	# Get the most recent time for the match.
-	time=`sudo grep -m1 "$M".*"$i".*"$U" /var/log/squid3/access.log | awk '{print $1}' | cut -d. -f1`
+	time=`sudo tac /var/log/squid3/access.log |  grep -m1 "$M".*"$i".*"$U" | awk '{print $1}' | cut -d. -f1`
 	# Convert the time from epoxh to Human readable format.
 	echo `date -d @$time` $i
 done
