1b463336b998dc79299ab215cb29dae302d32262
Qaisar <qaisar.sardar@gmail.com>
2020-03-30 13:29:18 +1100

    Add pause time of 30 minutes.
    The session will be paused after every 30 minutes for a total of 10
    minutes before the user is allowed to login back. The pause time will
    be universal (/var/log/pause_time) because mostly the kids share the
    screen and we want all the kids to wait before logging back on.

--- lock-screen-timer.sh
+++ lock-screen-timer.sh
@@ -4,15 +4,13 @@
 # PATH: $HOME/bin
 # DESC: Lock screen in x minutes
 # CALL: Place on Desktop or call from Terminal with "lock-screen-timer 99"
-# DATE: Created Nov 19, 2016. Last revision May 30, 2018.
+# DATE: Created Nov 19, 2016. Last revision Mar 29, 2020.
 # UPDT: Updated to support WSL (Windows Subsystem for Linux)
-#       Remove hotplugtv. Replace ogg with paplay.
 #       Cohesion with multi-timer. New sysmonitor indicator style.
 
 # NOTE: Time defaults to 60 minutes.
 #       If previous version is sleeping it is killed.
-#       Pending lock warning displayed on-screen at set intervals.
-#       Write time remaining to ./.lock-screen-timer-remaining
+#       Write time remaining to ./times/YYYY_MM_DD.time
 
 function isWeekend() {
 	d=$(date +%a)
@@ -89,8 +87,61 @@ function isBefore9Am() {
 }
 
 
+function readPauseTime () {
+        pt_file="/var/log/pause_time"
+        today=$(today_fn)
+	d=$(grep $today $pt_file)
+	
+	echo $(echo $d | cut -d " " -f 2)
+}
+
+function today_fn () {
+	command date +%Y_%m_%d
+}
+
+function isPauseTimeOn () {
+   	t=$(readPauseTime)
+	nt=$(( $t + 600 ))
+	ct=$(date +%s)
+
+	echo "reading pause time $t" $(date --date=@$t)
+	echo "current time $t" $(date --date=@$ct)
+	echo "next time $t" $(date --date=@$nt)
+
+	if [[ $t ]];then
+		if [[ "$ct" -le "$nt" ]]; then
+			echo "ct is less than nt - should pause now"
+	   		return 0;
+		fi
+		echo "ct is more than nt - not pause"
+		return 1;
+   	else
+		echo "ct is more than nt - not pause"
+	       	return 1;
+	fi
+}
+
+function writePauseTime () {
+        pt_file="/var/log/pause_time"
+        today=$(today_fn)
+	time=$(date +%s)
+	human_time=$(date --date=@$time)
+
+	# Look for todays date in the pause time file.
+	d=$(grep $t $pt_file)
+	if [[ $d ]]; then
+		# If found, replace the time.
+		echo "writing new pause time $human_time"
+		sudo sed -i "s/^$today.*/$today $time $human_time/g" $pt_file
+	else
+		#If not found, just add a new line.
+		echo "first time writing pause time $human_time"
+		sudo echo "$today $time $human_time" >> $pt_file
+	fi
+}
 
 MINUTES="$1" # Optional parameter 1 when invoked from terminal.
+ARG_MINUTES="$1"
 DISPLAY=$(who | egrep $THIS_USER\\s+: | awk '{print $2}')
 # if no parameters set default MINUTES to 30
 if [ $# == 0 ]; then
@@ -107,6 +158,7 @@ for i in "$@"; do
 	if [[ $i =~ ^minutes=[0-9]+ ]]; then
 		IFS=’=’ read -ra MINS <<< "$i"
 		MINUTES="${MINS[1]}"
+		ARG_MINUTES="${MINS[1]}"
 	fi
 	if [[ $i =~ ^sleep=[0-9]+ ]]; then
 		IFS=’=’ read -ra ARR <<< "$i"
@@ -161,6 +213,8 @@ while true ; do # loop until cancel
     if isBefore9Am; then
       logOutOrShutdown
     fi
+
+
     # If run in AUTO mode read minutes from todays file if exists
     # or create todays file and put minutes into it.
     MINUTES=$(readFromToday $MINUTES)
@@ -169,14 +223,32 @@ while true ; do # loop until cancel
     # Loop for X minutes, testing each minute for alert message.
     #(( ++MINUTES )) 
     while (( $MINUTES > 0 )); do
-        case $MINUTES in 1|2|3|5|10|15|30|45|60|120|480|960|1920)
-            if [[ $WSL_running == true ]]; then  
-                powershell.exe -c '(New-Object Media.SoundPlayer "C:\Windows\Media\notify.wav").PlaySync();'
-            else
-               paplay /usr/share/sounds/freedesktop/stereo/complete.oga ;
-            fi
-	    notify-send -t 1 --urgency=critical --icon=/usr/share/icons/gnome/256x256/status/appointment-soon.png "Locking screen in ""$MINUTES"" minute(s)." ;
-           ;;
+
+       if isPauseTimeOn; then
+          logOutOrShutdown
+       fi
+
+       case $MINUTES in 
+		1|2|3|5|10|15|30|45|60|90|120|480|960|1920)
+            		paplay /usr/share/sounds/freedesktop/stereo/complete.oga ;
+	    		notify-send -t 1 --urgency=critical --icon=/usr/share/icons/gnome/256x256/status/appointment-soon.png "Locking screen in ""$MINUTES"" minute(s)." ;
+
+			case $MINUTES in 30|60|90)
+				# If the current minutes are not equal to the 
+				# initially provided minutes as argments, then
+				# initiate the pause time. This means that its already been 30 minutes.
+				# This assumes that the maximum time provided is 120 minutes.
+				if [ $ARG_MINUTES -ne $MINUTES ]; then
+					writePauseTime
+				fi
+				;;
+			esac
+			;;
+		32|62|92)
+		    	paplay /usr/share/sounds/freedesktop/stereo/complete.oga ;
+		    	notify-send -t 1 --urgency=critical --icon=/usr/share/icons/gnome/256x256/status/appointment-soon.png "10 minute pause time in 2 minute(s)." ;
+			echo "Second case"
+			;;
         esac;
         # Record number of minutes remaining to file other processes can read.
         echo "Lock screen in: $MINUTES Minutes" > ~/.lock-screen-timer-remaining
